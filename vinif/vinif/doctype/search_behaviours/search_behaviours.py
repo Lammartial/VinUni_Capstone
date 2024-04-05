1b8f049c2eb02f3d7db38a4b13d7113bfba2fd00
Lam Vo <20lam.vkt@vinuni.edu.vn>
2024-04-05 11:32:56 +0800

    Add custom frappe app

---
+++ vinif/vinif/doctype/search_behaviours/search_behaviours.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, minh and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class search_behaviours(Document):
+	pass
